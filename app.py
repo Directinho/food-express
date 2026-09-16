8373d279b31f022dabffb8e35aa4c67c691113f2
DirectDev <jcmassoco2@gmail.com>
2026-09-16 15:52:49 -0300

    Aula de Loop For e Listas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 
+restaurantes = ['Patos Food', 'Mac Ronalds']
 def exibir_nome_do_programa():
     print("""
     ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -23,13 +24,29 @@ def opcao_invalida():
     print('Opção Inválida\n')
     input('Digite uma tecla para voltar ao menu principal: ')
     main()
+
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de Novos restaurantes\n')
+    nome_do_restaurante = input('Insira o nome do restaurante para ser cadastrado: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso')
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando os restaurantes\n')
+    for restaurante in restaurantes:
+        print(f'-{restaurante}')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 def escolher_opcao():
     try:
         opcao = int(input('Insira uma opção: '))
         if opcao == 1:
-            print('Cadastrar restaurante')
+            cadastrar_novo_restaurante()
         elif opcao == 2:
-            print('Listar Restaurantes')
+            listar_restaurantes()
         elif opcao == 3:
             print('Ativar Restaurante')
         elif opcao == 4:
